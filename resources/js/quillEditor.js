ba05eab364d690c0a1094636b064b3452b7ee79d
Rumon <crash.msr@gmail.com>
2020-04-09 16:34:30 +0600

    changed quill css import from cdn to local

--- resources/js/quillEditor.js
+++ resources/js/quillEditor.js
@@ -1,19 +1,18 @@
-var Quill = require("quill");
+const Quill = require("quill");
 const ImageResize = require("quill-image-resize");
-// console.log(ImageResize);
 
-// Quill.register("modules/imageResize", ImageResize);
+Quill.register("modules/imageResize", ImageResize);
 
-var quill = new Quill("#editor", {
+const quill = new Quill("#editor", {
     modules: {
-        // imageResize: {},
+        imageResize: {},
         toolbar: [
             [{ header: [2, 3, false] }],
             ["bold", "italic", "underline", "strike"],
             [{ list: "ordered" }, { list: "bullet" }],
             [{ align: [] }],
-            ["image", "link"]
-        ]
+            ["image", "link"],
+        ],
     },
-    theme: "snow"
+    theme: "snow",
 });
